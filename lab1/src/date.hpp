a78df22bb1ac9a4b9c37ea47002c2f007b35763c
Alex-pvl <74912588+Alex-pvl@users.noreply.github.com>
2021-09-26 14:31:24 +0700

    first

---
+++ lab1/src/date.hpp
@@ -0,0 +1,326 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Date {
+
+private:
+    int year;
+    int mon;
+    int day;
+    int h;
+    int m;
+    int s;
+    char* date_time;
+    static int count;
+
+public:
+    int getYear() { return this->year; }
+
+    int getMonth() { return this->mon; }
+
+    int getDay() { return this->day; }
+
+    int getHour() { return this->h; }
+
+    int getMinute() { return this->m; }
+
+    int getSecond() { return this->s; }
+
+    char* getDateTime() { return this->date_time; }
+
+    static int getCount() { return count; }
+
+    void setYear(int year) { this->year = year; }
+
+    void setMonth(int mon) { this->mon = mon; }
+
+    void setDay(int day) { this->day = day; }
+
+    void setHour(int h) { this->h = h; }
+ 
+    void setMinute(int m) { this->m = m; }
+
+    void setSecond(int s) { this->s = s; }
+
+    int getSize(int n) {
+        int size = 0;
+        for (int i = n; i > 0; i /= 10) {
+            size++;
+        }
+        return size;
+    }
+
+    void incYear() {
+        this->setYear(this->getYear() + 1);
+    }
+
+    void incMonth() {
+        if (this->getMonth() == 12) {
+            this->incYear();
+            this->setMonth(1);
+        } else {
+            this->setMonth(this->getMonth() + 1);
+        }
+    }
+
+    void incDay() {
+        if ((this->getMonth() == 4 || this->getMonth() == 6 ||
+        this->getMonth() == 9 || this->getMonth() == 11) && this->getDay() == 30) {
+            this->incMonth();
+            this->setDay(1);
+        } else if (this->getMonth() == 2) {
+            if (this->getYear() % 4 == 0) {
+                if (this->getYear() % 100 == 0 && this->getYear() % 400 != 0) {
+                    if (this->getDay() == 28) {
+                        this->incMonth();
+                        this->setDay(1);
+                    } else {
+                        this->setDay(this->getDay() + 1);
+                    }
+                } else {
+                    if (this->getDay() == 29) {
+                        this->incMonth();
+                        this->setDay(1);
+                    } else {
+                        this->setDay(this->getDay() + 1);
+                    }
+                }
+            } else if (this->getDay() == 28) {
+                this->incMonth();
+                this->setDay(1);
+            } else {
+                this->setDay(this->getDay() + 1);
+            }
+        } else if (this->getDay() == 31) {
+            this->incMonth();
+            this->setDay(1);
+        } else {
+            this->setDay(this->getDay() + 1);
+        }
+    }
+
+    void incHour() {
+        if (this->getHour() == 23) {
+            this->incDay();
+            this->setHour(0);
+        } else {
+            this->setHour(this->getHour() + 1);
+        }
+    }
+
+    void incMinutes() {
+        if (this->getMinute() == 59) {
+            this->incHour();
+            this->setMinute(0);
+        } else {
+            this->setMinute(this->getMinute() + 1);
+        }
+    }
+
+    void incSecond() {
+        if (this->getSecond() == 59) {
+            this->incMinutes();
+            this->setSecond(0);
+        } else {
+            this->setSecond(this->getSecond() + 1);
+        }
+    }
+
+    void decYear() {
+        this->setYear(this->getYear() - 1);
+    }
+
+    void decMonth() {
+        if (this->getMonth() == 1) {
+            this->decYear();
+            this->setMonth(12); 
+        } else {
+            this->setMonth(this->getMonth() - 1);
+        }
+    }
+
+    void decDay() {
+        if ((this->getMonth() == 5 || this->getMonth() == 7 ||
+        this->getMonth() == 10 || this->getMonth() == 12) && this->getDay() == 1) {
+            this->decMonth();
+            this->setDay(30);
+        } else if (this->getMonth() == 3) {
+            if (this->getYear() % 4 == 0) {
+                if (this->getYear() % 100 == 0 && this->getYear() % 400 != 0) {
+                    if (this->getDay() == 1) {
+                        this->decMonth();
+                        this->setDay(28);
+                    } else {
+                        this->setDay(this->getDay() - 1);
+                    }
+                } else {
+                    if (this->getDay() == 1) {
+                        this->decMonth();
+                        this->setDay(29);
+                    } else {
+                        this->setDay(this->getDay() - 1);
+                    }
+                }
+            } else if (this->getDay() == 1) {
+                this->decMonth();
+                this->setDay(28);
+            } else {
+                this->setDay(this->getDay() - 1);
+            }
+        } else if (this->getDay() == 1) {
+            this->decMonth();
+            this->setDay(31);
+        } else {
+            this->setDay(this->getDay() - 1);
+        }
+    } 
+
+    void decHour() {
+        if (this->getHour() == 0) {
+            this->decDay();
+            this->setHour(23);
+        } else {
+            this->setHour(this->getHour() - 1);
+        }
+    }
+
+    void decMinute() {
+        if (this->getMinute() == 0) {
+            this->decHour();
+            this->setMinute(59);
+        } else {
+            this->setMinute(this->getMinute() - 1);
+        }
+    }
+
+    void decSecond() {
+        if (this->getSecond() == 0) {
+            this->decMinute();
+            this->setSecond(59);
+        } else {
+            this->setSecond(this->getSecond() - 1);
+        }
+    }
+
+    int i = 0;
+    char* setDateTime() {
+        int year = this->year, mon = this->mon, day = this->day, h = this->h, m = this->m, s = this->s;
+        int year_s = getSize(year), year_p = pow(10, year_s-1); 
+        int mon_s = getSize(mon), mon_p = pow(10, mon_s-1);
+        int day_s = getSize(day), day_p = pow(10, day_s-1);
+        int h_s = getSize(h), h_p = pow(10, h_s-1);
+        int m_s = getSize(m), m_p = pow(10, m_s-1);
+        int s_s = getSize(s), s_p = pow(10, s_s-1);
+        
+        for (int dy = day; day_p > 0; day_p /= 10) {
+            if (day / 10 == 0) {
+                date_time[i++] = '0';
+            }
+            date_time[i++] = (char) (dy / day_p + '0');
+            dy %= day_p;
+        }
+        date_time[i++] = '.';
+        for (int mn = mon; mon_p > 0; mon_p /= 10) {
+            if (mon / 10 == 0) {
+                date_time[i++] = '0';
+            } 
+            date_time[i++] = (char) (mn / mon_p + '0');
+            mn %= mon_p;
+        }
+        date_time[i++] = '.';
+        for (int yr = year; year_p > 0; year_p /= 10) {
+            date_time[i++] = (char) (yr / year_p + '0');
+            yr %= year_p;
+        }
+        date_time[i++] = ' ';
+        if (h == 0) {
+            date_time[i++] = (char) '0';
+            date_time[i++] = (char) '0';
+        } 
+        for (int hour = h; h_p > 0; h_p /= 10) {
+            if (h / 10 == 0) {
+                date_time[i++] = (char) '0';
+            } 
+            date_time[i++] = (char) (hour / h_p + '0');
+            hour %= h_p;
+        }
+        date_time[i++] = ':';
+        if (m == 0) {
+            date_time[i++] = (char) '0';
+            date_time[i++] = (char) '0';
+        } 
+        for (int min = m; m_p > 0; m_p /= 10) {
+            if (m / 10 == 0) {
+                date_time[i++] = (char) '0';
+            } 
+            date_time[i++] = (char) (min / m_p + '0');
+            min %= m_p;
+        }
+        date_time[i++] = ':';
+        if (s == 0) {
+            date_time[i++] = (char) '0';
+            date_time[i] = (char) '0';
+        } 
+        for (int sec = s; s_p > 0; s_p /= 10) {
+            if (s / 10 == 0) {
+                date_time[i++] = (char) '0';
+            } 
+            date_time[i] = (char) (sec / s_p + '0');
+            sec %= s_p;
+        }
+        return date_time;
+    }
+    
+    void toString() {
+        this->setDateTime();
+        for (int j = 0; j <= i; j++) {
+            cout << this->getDateTime()[j];
+        }
+        cout << endl;
+    }
+
+    Date() {
+        count++;
+    }
+
+    Date(int day, int mon, int year) {
+        this->date_time = new char[i];
+        this->setDay(day);
+        this->setMonth(mon);
+        this->setYear(year);
+        this->setHour(0);
+        this->setMinute(0);
+        this->setSecond(0);
+        count++;
+    }
+
+    Date(int day, int mon, int year, int h, int m, int s) {
+        this->date_time = new char[i];
+        this->setDay(day);
+        this->setMonth(mon);
+        this->setYear(year);
+        this->setHour(h);
+        this->setMinute(m);
+        this->setSecond(s);
+        count++;
+    }
+
+    Date(const Date &d) {
+        this->setDay(d.day);
+        this->setMonth(d.mon);
+        this->setYear(d.year);
+        this->setHour(d.h);
+        this->setMinute(d.m);
+        this->setSecond(d.s);
+        this->date_time = d.date_time;
+        count++;
+    }
+
+    ~Date() {
+        delete[] date_time;
+        count--;
+    }
+};
+
+int Date::count = 0;
---
+++ lab1/tests/test1.cpp
@@ -0,0 +1,127 @@
+#define CATCH_CONFIG_MAIN
+
+#include "../src/date.hpp"
+#include "catch.hpp"  
+
+// increment testing
+TEST_CASE("Testing incYear()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incYear();
+    REQUIRE(d.getYear() == 2022);
+}
+
+TEST_CASE("Testing incMonth()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incMonth();
+    REQUIRE(d.getMonth() == 1);
+    REQUIRE(d.getYear() == 2022);
+}
+
+TEST_CASE("Testing incDay()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incDay();
+    REQUIRE(d.getDay() == 1);
+    REQUIRE(d.getMonth() == 1);
+    REQUIRE(d.getYear() == 2022);
+}
+
+TEST_CASE("Testing incHour()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incHour();
+    REQUIRE(d.getHour() == 0);
+    REQUIRE(d.getDay() == 1);
+    REQUIRE(d.getMonth() == 1);
+    REQUIRE(d.getYear() == 2022);
+}
+
+TEST_CASE("Testing incMinutes()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incMinutes();
+    REQUIRE(d.getMinute() == 0);
+    REQUIRE(d.getHour() == 0);
+    REQUIRE(d.getDay() == 1);
+    REQUIRE(d.getMonth() == 1);
+    REQUIRE(d.getYear() == 2022);
+}
+
+TEST_CASE("Testing incSecond()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    d.incSecond();
+    REQUIRE(d.getSecond() == 0);
+    REQUIRE(d.getMinute() == 0);
+    REQUIRE(d.getHour() == 0);
+    REQUIRE(d.getDay() == 1);
+    REQUIRE(d.getMonth() == 1);
+    REQUIRE(d.getYear() == 2022);
+}
+
+// decrement testing
+TEST_CASE("Testing decYear()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decYear();
+    REQUIRE(d.getYear() == 1999);
+}
+
+TEST_CASE("Testing decMonth()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decMonth();
+    REQUIRE(d.getMonth() == 2);
+}
+
+TEST_CASE("Testing decDay()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decDay();
+    REQUIRE(d.getDay() == 29);
+    REQUIRE(d.getMonth() == 2);
+}
+
+TEST_CASE("Testing decHour()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decHour();
+    REQUIRE(d.getHour() == 23);
+    REQUIRE(d.getDay() == 29);
+    REQUIRE(d.getMonth() == 2);
+}
+
+TEST_CASE("Testing decMinutes()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decMinute();
+    REQUIRE(d.getMinute() == 59);
+    REQUIRE(d.getHour() == 23);
+    REQUIRE(d.getDay() == 29);
+    REQUIRE(d.getMonth() == 2);
+}
+
+TEST_CASE("Testing decSecond()") {
+    Date d(1, 3, 2000, 0, 0, 0);
+    d.decSecond();
+    REQUIRE(d.getSecond() == 59);
+    REQUIRE(d.getMinute() == 59);
+    REQUIRE(d.getHour() == 23);
+    REQUIRE(d.getDay() == 29);
+    REQUIRE(d.getMonth() == 2);
+}
+
+// count testing 
+TEST_CASE("Testing getCount()") {
+    Date d1(1,1,1);
+    Date d2(2,2,2);
+    Date d3(3,3,3);
+    Date d4(4,4,4);
+    REQUIRE(d1.getCount() == 4);
+}
+
+// date_time testing
+TEST_CASE("Testing getDateTime()") {
+    Date d(31, 12, 2021, 23, 59, 59);
+    char* arr = new char[19] {'0', '1', '.', '0', '1', '.', '2', '0', '2', '2', ' ', '0', '0', ':', '0', '0', ':', '0', '0'};
+    d.incSecond();
+    d.setDateTime();
+    bool isEqual = true;
+    for (int i = 0; i < 19; i++) {
+        if (arr[i] != d.getDateTime()[i]) {
+            isEqual = false;
+        }
+    } 
+    REQUIRE(isEqual); 
+}
